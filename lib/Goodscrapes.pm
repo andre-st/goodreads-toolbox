c45f0a78b577e0cb0070c77b2bb581bb48dfef5a
andre-st <datakadabra@gmail.com>
2019-01-31 22:45:20 +0100

    Clean up

--- lib/Goodscrapes.pm
+++ lib/Goodscrapes.pm
@@ -2091,7 +2091,6 @@ sub _setcurlopts
 	# 
 	# The module works without any of these options, but probably slower.
 	# All `eval` due to https://github.com/andre-st/goodreads/issues/20
-	eval{ $curl->setopt( $curl->CURLOPT_BULLSHIT,       60  ); };
 	eval{ $curl->setopt( $curl->CURLOPT_TIMEOUT,        60  ); };
 	eval{ $curl->setopt( $curl->CURLOPT_CONNECTTIMEOUT, 60  ); };
 	eval{ $curl->setopt( $curl->CURLOPT_FORBID_REUSE,   0   ); };  # CURL default
